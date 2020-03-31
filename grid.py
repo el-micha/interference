d47576834d8412f48cf3b82e07b0d6e2dfac4481
el-micha <michael.weigelt@bluewin.ch>
2020-03-31 11:33:48 +0200

    Simple hacky noise generation for tile placement.

--- grid.py
+++ grid.py
@@ -2,7 +2,7 @@
 import random
 import os
 import pygame
-
+import matplotlib.pyplot as plt
 
 class Tilemap:
     """
@@ -42,8 +42,15 @@ class TileGrid:
 
     def __init__(self, width, height):
         # grid holds tile IDs
+
+
         tiles = [13,14,25,26]
-        self.grid = [[random.choice(tiles) for x in range(width)] for y in range(height)]
+        tiles = [2, 3, 15, 25, 26]
+
+        grid = smooth_noise(width, height, 5)
+        self.grid = map_noise_to_ids(grid, tiles)
+
+        #self.grid = [[random.choice(tiles) for x in range(width)] for y in range(height)]
         self.tilemap = Tilemap("art")
         self.tile_size = 32 # todo: get this from tilemap obj
 
@@ -57,6 +64,77 @@ class TileGrid:
         for line in self.grid:
             s += " ".join(map(str, line)) + "\n"
         return s
+
+
+def smooth(inp):
+    newgrid = [[0 for x in inp[0]] for y in inp]
+    for i in range(len(inp)):
+        for j in range(len(inp[0])):
+            neighbours = [inp[i][j]]
+            factor = 0.5
+            try:
+                neighbours.append(factor * inp[i-1][j])
+            except:
+                pass
+            try:
+                neighbours.append(factor * inp[i+1][j])
+            except:
+                pass
+            try:
+                neighbours.append(factor * inp[i][j-1])
+            except:
+                pass
+            try:
+                neighbours.append(factor * inp[i][j+1])
+            except:
+                pass
+            newgrid[i][j] = sum(neighbours) / (1 + (len(neighbours)-1)*factor)
+    return newgrid
+
+def disp(inp):
+    s = ""
+    for line in inp:
+        s += " ".join(map(str, line)) + "\n"
+    print(s)
+
+
+def grid_max(grid):
+    maximum = 0
+    for line in grid:
+        for x in line:
+            maximum = max(maximum, x)
+    return maximum
+
+def map_noise_to_ids(grid, ids):
+    # partition [0,max] into equally sized intervals, one interval for each id in ids.
+    maxm = grid_max(grid)
+    disp(grid)
+    print(maxm)
+    num_intervals = len(ids)
+    interval_size = maxm / num_intervals
+    interval_borders = [interval_size * (1+x) for x in range(num_intervals)]
+    def get_index(x, borders):
+        for i,b in enumerate(borders):
+            if x <= b:
+                return i
+        return len(borders)-1
+    newgrid = [[0 for x in grid[0]] for y in grid]
+    for i, line in enumerate(grid):
+        for j, elem in enumerate(line):
+            newgrid[i][j] = ids[get_index(elem, interval_borders)]
+    return newgrid
+
+def smooth_noise(width, height, iterations):
+    grid = [[random.expovariate(0.1) for x in range(width)] for y in range(height)]
+    #plt.imshow(grid)
+    #plt.show()
+    for i in range(iterations):
+        grid = smooth(grid)
+        #plt.imshow(grid)
+        #plt.show()
+    return grid
+
+
 
 
 # tests
